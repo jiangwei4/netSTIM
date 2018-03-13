451b84eb9f8e5e645de0693cd7fef67f43c79feb
donovan* <dono.rock@hotmail.fr>
2018-03-13 23:31:14 +0100

    fin

--- ajouterModificationProduits.php
+++ ajouterModificationProduits.php
@@ -11,7 +11,7 @@ $details = $_GET['details'];
 $con = mysqli_connect('localhost', $login , $password , 'BDD') or die ('erreur de connexion au serveur');
 
 
-mysqli_query($con, "UPDATE Produits SET quantité='$quantite'  and details='$details' WHERE nom='$nom'") or die ('Erreur lors de l\'envoi');
-
+mysqli_query($con, "UPDATE Produits SET details='$details' WHERE nom='$nom'") or die ('Erreur lors de l\'envoi');
+mysqli_query($con, "UPDATE Produits SET quantite='$quantite' WHERE nom='$nom'") or die ('Erreur lors de l\'envoi a cause de l accent a quantité qu il faut que je change dans la base de donnée');
 echo '<h1>produit modifié merci de recharger la page</h1>';
 ?>
